68a1fe5403bfb5f4416c645d30d3b3b5ae87a174
unknown <khomanesuraj8@gmail.com>
2021-12-20 14:31:51 +0530

    Return JSON for different http methods

--- src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
@@ -10,17 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.greeting.model.Greeting;
 
+/**
+ * @RestController
+ * @RequestMapping: mapping web requests onto methods in request-handling
+ *                  classes with flexible method signatures.
+ */
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	/**
+	 * @GetMapping: mapping HTTP GET requests onto specific handler methods
+	 * @RequestParam: method parameter should be bound to a web request parameter.
+	 */
 	@GetMapping(value = { "", "/", "/home" })
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
+	/**
+	 * @GetMapping: mapping HTTP GET requests onto specific handlermethods
+	 * @PathVariable: method parameter should be bound to a URI templatevariable.
+	 *                Supported for RequestMapping annotated handler methods.
+	 */
 	@GetMapping("/{name}")
 	public Greeting greetings(@PathVariable String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
